099c1d0d9adf34fcc521d99d6bdcc29df64029f2
Jayashakthi Vishnu <vishnu28.js@gmail.com>
2023-01-13 17:52:58 +0530

    add user register checker

---
+++ utils/isRegistered.py
@@ -0,0 +1,15 @@
+from functools import wraps
+from flask import request
+from utils.db import findOne
+
+def is_registerd(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        email=request.headers.get("email");
+        print(email)
+        res=findOne("user",{"email":email})
+        if not res:
+            resp={"status":"not registered"}
+            return resp,200
+        return f(email,*args, **kwargs)
+    return decorated                                                                       
