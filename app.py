5080bae9b3b31cf80537d2aa4a46fe5b9bc89903
Jayashakthi Vishnu <vishnu28.js@gmail.com>
2023-01-13 19:08:05 +0530

    add main app file

---
+++ app.py
@@ -0,0 +1,28 @@
+import json
+import os
+from flask import Flask
+from flask_restful import Api
+from routes.Auth import Register
+from routes.Post import Post
+from routes.Topic import Topic
+from routes.IsRegistered import IsRegistered
+from routes.ResendVerifyEmail import Resend,Status
+from flask_cors import CORS
+
+app=Flask(__name__)
+CORS(app=app,supports_credentials=True)
+api=Api(app)
+
+
+app.secret_key = 'the random string'
+
+
+api.add_resource(Register,"/register")
+api.add_resource(Post,"/post")
+api.add_resource(Topic,"/tags")
+api.add_resource(IsRegistered,"/isregister")
+api.add_resource(Resend,"/email/verify")
+api.add_resource(Status,"/email/check")
+
+if __name__=="__main__":
+    app.run(debug=True)
