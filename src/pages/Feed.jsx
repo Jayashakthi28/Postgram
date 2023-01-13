14b98d67d7afd083d2c5b380f6e52c2885f38394
Jayashakthi Vishnu <vishnu28.js@gmail.com>
2023-01-13 19:12:50 +0530

    move redirect logic to app.jsx

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import Loading from "../components/Loading";
+import { api } from "../utils/api";
 
 export default function Feed() {
-  return (
-    <div>Feed</div>
-  )
+  const [apiData, setapiData] = useState({ loading: true });
+  useEffect(() => {
+    async function fetchFeed() {
+      let apiResponse = await api.get("/post");
+      apiResponse.loading = false;
+      setapiData(apiResponse);
+    }
+    fetchFeed();
+  }, []);
+  if (apiData.loading) {
+    return <Loading />;
+  }
+  return <div>Feed</div>;
 }
