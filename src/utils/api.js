480097e829a1071e020ea3806a4df6f2fd2e94eb
Jayashakthi Vishnu <vishnu28.js@gmail.com>
2023-01-13 19:14:52 +0530

    add map for routes

--- src/utils/api.js
+++ src/utils/api.js
@@ -20,17 +20,29 @@ const API = () => {
   };
   const post = async (endpoint, payload) => {
     let data = await axios.post(`${url}/${endpoint}`, payload);
-    data=data["data"];
-    console.log(data);
+    data = data["data"];
     return data;
   };
   const get = async (endpoint) => {
     let data = await axios.get(`${url}/${endpoint}`);
-    data=data["data"];
-    console.log(data);
+    data = data["data"];
     return data;
   };
-  return { get, post, setEmail, setUserData, getUserData };
+  const routeJson = {
+    "/register": false,
+    "/verify": false,
+    "/": true,
+    "/search": true,
+    "/notification": true,
+    "/profile": true,
+  };
+  const headerJson={
+    "/":"home",
+    "/search":"search",
+    "/notification":"notification",
+    "/profile":"profile"
+  }
+  return { get, post, setEmail, setUserData, getUserData,routeJson,headerJson };
 };
 
 export const api = API();
