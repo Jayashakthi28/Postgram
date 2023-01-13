0b5f1dccc524041fca50d57e635d5d81bfd7407b
Jayashakthi Vishnu <vishnu28.js@gmail.com>
2023-01-13 06:58:25 +0530

    import material ui

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,29 @@
 import { Auth0Provider } from "@auth0/auth0-react";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
+import { createTheme, ThemeProvider } from "@mui/material";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: ["Bubbler"].join(","),
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Auth0Provider
-    domain={import.meta.env.VITE_AUTH_DOMAIN}
-    clientId={import.meta.env.VITE_AUTH_CLIENT_ID}
-    redirectUri={window.location.origin}
-  >
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Auth0Provider>
+  <BrowserRouter>
+    <Auth0Provider
+      domain={import.meta.env.VITE_AUTH_DOMAIN}
+      clientId={import.meta.env.VITE_AUTH_CLIENT_ID}
+      redirectUri={window.location.origin}
+    >
+      <ThemeProvider theme={theme}>
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+      </ThemeProvider>
+    </Auth0Provider>
+  </BrowserRouter>
 );
