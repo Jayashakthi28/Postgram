53eae01c1fa86ebcc78fd7d1aff9e73dd328c5c5
Jayashakthi Vishnu <vishnu28.js@gmail.com>
2023-01-13 19:11:29 +0530

    change tab focus on route

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
 import Typography from "@mui/material/Typography";
@@ -6,12 +6,17 @@ import HomeIcon from "@mui/icons-material/Home";
 import SearchIcon from "@mui/icons-material/Search";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 import PersonIcon from "@mui/icons-material/Person";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
+import { api } from "../utils/api";
 
 export default function Header({ headerValue, setHeaderValue }) {
   const navigate = useNavigate();
-
+  const location = useLocation();
+  const isNav = api.routeJson[location.pathname];
+  useEffect(() => {
+    setHeaderValue(api.headerJson[location.pathname] || "home");
+  }, [location.pathname]);
   return (
     <>
       <div className=" backdrop-blur-md bg-purple-300">
@@ -24,42 +29,44 @@ export default function Header({ headerValue, setHeaderValue }) {
         >
           PostGram!
         </Typography>
-        <Tabs value={headerValue} centered>
-          <Tab
-            value="home"
-            icon={<HomeIcon fontSize="large" className=" text-black" />}
-            onClick={() => {
-              setHeaderValue("home");
-              navigate("/");
-            }}
-          />
-          <Tab
-            value="search"
-            icon={<SearchIcon fontSize="large" className=" text-black" />}
-            onClick={() => {
-              setHeaderValue("search");
-              navigate("/search");
-            }}
-          />
-          <Tab
-            value="notification"
-            icon={
-              <NotificationsIcon fontSize="large" className=" text-black" />
-            }
-            onClick={() => {
-              setHeaderValue("notification");
-              navigate("/notification");
-            }}
-          />
-          <Tab
-            value="profile"
-            icon={<PersonIcon fontSize="large" className=" text-black" />}
-            onClick={() => {
-              setHeaderValue("profile");
-              navigate("/profile");
-            }}
-          />
-        </Tabs>
+        {isNav && (
+          <Tabs value={headerValue} centered>
+            <Tab
+              value="home"
+              icon={<HomeIcon fontSize="large" className=" text-black" />}
+              onClick={() => {
+                setHeaderValue("home");
+                navigate("/");
+              }}
+            />
+            <Tab
+              value="search"
+              icon={<SearchIcon fontSize="large" className=" text-black" />}
+              onClick={() => {
+                setHeaderValue("search");
+                navigate("/search");
+              }}
+            />
+            <Tab
+              value="notification"
+              icon={
+                <NotificationsIcon fontSize="large" className=" text-black" />
+              }
+              onClick={() => {
+                setHeaderValue("notification");
+                navigate("/notification");
+              }}
+            />
+            <Tab
+              value="profile"
+              icon={<PersonIcon fontSize="large" className=" text-black" />}
+              onClick={() => {
+                setHeaderValue("profile");
+                navigate("/profile");
+              }}
+            />
+          </Tabs>
+        )}
       </div>
       <Outlet />
     </>
