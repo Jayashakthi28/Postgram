2c3da9c9775f52b260dc04fc55ccb4a1033600bf
Jayashakthi Vishnu <vishnu28.js@gmail.com>
2023-01-24 13:58:17 +0530

    add media query config

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -5,6 +5,23 @@ module.exports = {
     extend: {
       fontFamily: {
         bubbler: ["Bubbler"],
+        patrick: ["Patrick"],
+        pacifico: ["Pacifico"],
+        righteous: ["Righteous"],
+        sofiasans: ["Sofiasans"],
+      },
+      screens: {
+        sm: { min: "0px", max: "767px" },
+        // => @media (min-width: 640px) { ... }
+
+        md: "768px",
+        // => @media (min-width: 768px) { ... }
+
+        lg: "1024px",
+        // => @media (min-width: 1024px) { ... }
+
+        xl: "1280px",
+        // => @media (min-width: 1280px) { ... }
       },
     },
   },
