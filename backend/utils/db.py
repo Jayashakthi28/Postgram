11575287c5561a698b75f4842a1763f9d6538d0b
Jayashakthi Vishnu <vishnu28.js@gmail.com>
2023-01-27 21:41:55 +0530

    add default iteration from result

--- backend/utils/db.py
+++ backend/utils/db.py
@@ -2,23 +2,27 @@ import pymongo
 from dotenv import load_dotenv
 import os
 
-client = pymongo.MongoClient('localhost',27017)
+client = pymongo.MongoClient('localhost', 27017)
 db = client['postgram']
 
-def insertOne(collection,data):
-    col=db[collection]
-    res=col.insert_one(data)
 
-def insertMany(collection,data):
-    col=db[collection]
-    res=col.insert_many(data)
+def insertOne(collection, data):
+    col = db[collection]
+    res = col.insert_one(data)
 
-def findOne(collection,data):
-    col=db[collection]
-    res=col.find_one(data)
+
+def insertMany(collection, data):
+    col = db[collection]
+    res = col.insert_many(data)
+
+
+def findOne(collection, data):
+    col = db[collection]
+    res = col.find_one(data)
     return res
 
-def findWithProject(collection,data,projection):
-    col=db[collection]
-    res=col.find(data,projection)
+
+def findWithProject(collection, data, projection):
+    col = db[collection]
+    res = list(col.find(filter=data, projection=projection))
     return res
