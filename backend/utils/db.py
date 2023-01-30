5a50db10a5215cb81ea10c7d5e73ecb68331baeb
Jayashakthi Vishnu <vishnu28.js@gmail.com>
2023-01-30 19:41:27 +0530

    add update function

--- backend/utils/db.py
+++ backend/utils/db.py
@@ -9,18 +9,27 @@ db = client['postgram']
 def insertOne(collection, data):
     col = db[collection]
     res = col.insert_one(data)
-
+    return res
 
 def insertMany(collection, data):
     col = db[collection]
     res = col.insert_many(data)
-
+    return res
 
 def findOne(collection, data):
     col = db[collection]
     res = col.find_one(data)
     return res
 
+def find(collection,data):
+    col=db[collection]
+    res=col.find(data)
+    return res
+
+def updateOne(collection,data,filter):
+    col=db[collection]
+    res=col.update_one(filter,data)
+    return res
 
 def findWithProject(collection, data, projection):
     col = db[collection]
