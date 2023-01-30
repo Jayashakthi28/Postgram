705c4124bbd59041b9a097d318139eecb0363ad1
Jayashakthi Vishnu <vishnu28.js@gmail.com>
2023-01-30 19:40:48 +0530

    add tag creation logic

--- backend/routes/Post.py
+++ backend/routes/Post.py
@@ -1,7 +1,7 @@
 from flask_restful import Resource
 from utils.isRegistered import is_registerd
 from flask import request
-from utils.db import findWithProject, findOne,insertOne
+from utils.db import findWithProject, findOne,insertOne,find,updateOne
 from datetime import datetime
 
 class Post(Resource):
@@ -30,5 +30,11 @@ class Post(Resource):
             "user": userId,
             "time":time
         }
-        insertOne("post",post_data);
+        res=insertOne("post",post_data);
+        postId=res.inserted_id
+        for x in tag:
+            ack=list(find("tags",{"topic":x}))
+            if(not ack):
+                insertOne("tags",{"topic":x,"posts":[]})
+            updateOne("tags",{"$push":{"posts":postId}},{"topic":x})
         return {"status":"Successfully inserted"},200;
