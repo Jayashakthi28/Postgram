dd5e3c7f43142263fa5e8c238b09289b386a5b03
Jayashakthi Vishnu <vishnu28.js@gmail.com>
2023-02-03 21:31:03 +0530

    add user profile route

--- backend/app.py
+++ backend/app.py
@@ -7,6 +7,7 @@ from routes.Post import Post
 from routes.Topic import Topic
 from routes.IsRegistered import IsRegistered
 from routes.ResendVerifyEmail import Resend,Status
+from routes.User import User
 from flask_cors import CORS
 
 app=Flask(__name__)
@@ -23,6 +24,6 @@ api.add_resource(Topic,"/tags")
 api.add_resource(IsRegistered,"/isregister")
 api.add_resource(Resend,"/email/verify")
 api.add_resource(Status,"/email/check")
-
+api.add_resource(User,"/user/<username>","/user")
 if __name__=="__main__":
     app.run(debug=True)
