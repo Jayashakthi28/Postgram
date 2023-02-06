ab8309ab5cc0d22733a4dc0258e8afbcf686ea7e
Jayashakthi Vishnu <vishnu28.js@gmail.com>
2023-02-06 21:36:04 +0530

    integrate follow unfollow and email to username route

--- backend/app.py
+++ backend/app.py
@@ -8,6 +8,9 @@ from routes.Topic import Topic
 from routes.IsRegistered import IsRegistered
 from routes.ResendVerifyEmail import Resend,Status
 from routes.User import User
+from routes.User import Following
+from routes.User import UnFollow
+from routes.UserName import UserName
 from flask_cors import CORS
 
 app=Flask(__name__)
@@ -24,6 +27,10 @@ api.add_resource(Topic,"/tags")
 api.add_resource(IsRegistered,"/isregister")
 api.add_resource(Resend,"/email/verify")
 api.add_resource(Status,"/email/check")
+api.add_resource(UserName,"/username");
 api.add_resource(User,"/user/<username>","/user")
+api.add_resource(Following,"/follow")
+api.add_resource(UnFollow,"/unfollow")
+
 if __name__=="__main__":
     app.run(debug=True)
