4a0577bc4779454a0c9680c046e5a028d63041c7
Jayashakthi Vishnu <vishnu28.js@gmail.com>
2023-02-11 20:56:27 +0530

    modified entire db logic with following and id insted of followers and username

--- backend/routes/User.py
+++ backend/routes/User.py
@@ -1,26 +1,35 @@
 from flask_restful import Resource
 from utils.isRegistered import is_registerd
-from utils.db import findOne,findWithProject,updateOne
+from utils.db import findOne, findWithProject, updateOne, find, aggregationQuery
 from flask import request
 
-class User(Resource):
 
+class User(Resource):
     @is_registerd
     def get(email, self, username=None):
         userData = {}
-        followingFlag=None
+        followingFlag = None
+        followers = []
+        myUserFollwing = findWithProject("user", {"email": email}, {
+                                         "following": 1})[0]["following"]
         if (username == None):
             userData = findOne("user", {"email": email})
+            followersCursor = find("user", {"following": userData["_id"]})
+            for x in followersCursor:
+                followers.append(x)
         else:
             userData = findOne("user", {"username": username})
-            if(not userData):
-                return {"status":"not found"},200
-            myUserName= findWithProject("user",{"email":email},{"username":1})
-            myUserName=myUserName[0]["username"]
-            if(userData.__contains__("followers") and myUserName in userData["followers"]):
-                followingFlag=True
+            if (not userData):
+                return {"status": "not found"}, 200
+            userID = findWithProject(
+                "user", {"username": username}, {"_id": 1})[0]["_id"]
+            followersCursor = find("user", {"following": userID})
+            for x in followersCursor:
+                followers.append(x)
+            if userID in myUserFollwing:
+                followingFlag = True
             else:
-                followingFlag=False
+                followingFlag = False
         mainData = {
             "name":
             userData['name'],
@@ -31,124 +40,129 @@ class User(Resource):
             "following":
             len(userData["following"]),
             "followers":
-            len(userData["followers"]),
-            "isFollowing":followingFlag
+            len(followers),
+            "isFollowing": followingFlag
         }
         return mainData, 200
 
 
 class Follow(Resource):
     @is_registerd
-    def post(email,self):
-        data=request.json
-        username=data["username"]
-        requestUserName=request.headers.get("username");
-        updateOne("user",{"$push":{"following":username}},{"username":requestUserName})
-        updateOne("user",{"$push":{"followers":requestUserName}},{"username":username})
-        return {"status":"success"},200
+    def post(email, self):
+        data = request.json
+        username = data["username"]
+        userID = findWithProject(
+            "user", {"username": username}, {"_id": 1})[0]["_id"]
+        updateOne("user", {"$push": {"following": userID}}, {"email": email})
+        return {"status": "success"}, 200
+
 
 class UnFollow(Resource):
     @is_registerd
-    def post(email,self):
-        data=request.json
-        username=data["username"]
-        requestUserName=request.headers.get("username");
-        updateOne("user",{"$pullAll":{"following":[username]}},{"username":requestUserName})
-        updateOne("user",{"$pullAll":{"followers":[requestUserName]}},{"username":username})
-        return {"status":"success"},200
+    def post(email, self):
+        data = request.json
+        username = data["username"]
+        userID = findWithProject(
+            "user", {"username": username}, {"_id": 1})[0]["_id"]
+        updateOne("user", {"$pullAll": {"following": [userID]}}, {
+                  "email": email})
+        return {"status": "success"}, 200
+
 
 class Followers(Resource):
     @is_registerd
-    def get(email,self,username=None):
-        myfollowing=findWithProject("user",{"email":email},{"following":1})
-        followers=[]
-        if(username!=None):
-            followers=findWithProject("user",{"username":username},{"followers":1})
+    def get(email, self, username=None):
+        myFollowing = findWithProject("user", {"email": email}, {
+                                      "following": 1})[0]["following"]
+        retArr = []
+        myFollowers = []
+        if username == None:
+            myFollowers = list(aggregationQuery(collection="user", match={"email": email}, lookup={"from": "user", "localField": "_id", "foreignField": "following", "as": "followersData", "pipeline": [
+                               {"$project": {"username": 1, "name": 1, "_id": 1}}]}, projection={"followersData": 1}))[0]["followersData"]
         else:
-            followers=findWithProject("user",{"email":email},{"followers":1})
-        retArr=[]
-        for x in followers[0]["followers"]:
-            userData=findWithProject("user",{"username":x},{"name":1})
-            temObj={
-                    "username":x,
-                    "name":userData[0]["name"]
-                }
-            if(x in myfollowing[0]["following"]):
-                temObj["isFollowing"]=True
+            myFollowers = list(aggregationQuery(collection="user", match={"username": username}, lookup={"from": "user", "localField": "_id", "foreignField": "following", "as": "followersData", "pipeline": [
+                               {"$project": {"username": 1, "name": 1, "_id": 1}}]}, projection={"followersData": 1}))[0]["followersData"]
+        for x in myFollowers:
+            temObj = {
+                "name": x["name"],
+                "username": x["username"]
+            }
+            if x["_id"] in myFollowing:
+                temObj["isFollowing"] = True
             else:
-                temObj["isFollowing"]=False
+                temObj["isFollowing"] = False
             retArr.append(temObj)
-        return {"followers":retArr},200
+        return {"followers": retArr}, 200
+
 
 class Following(Resource):
     @is_registerd
-    def get(email,self,username=None):
-        myfollowing=findWithProject("user",{"email":email},{"following":1})[0]["following"]
-        retArr=[]
-        if(username!=None):
-            following=findWithProject("user",{"username":username},{"following":1})
-            for x in following[0]["following"]:
-                name=findWithProject("user",{"username":x},{"name":1})[0]["name"]
-                temObj={
-                    "username":x,
-                    "name":name
-                }
-                if(x in myfollowing):
-                    temObj["isFollowing"]=True
-                else:
-                    temObj["isFollowing"]=False
-                retArr.append(temObj)
+    def get(email, self, username=None):
+        myFollowing = findWithProject("user", {"email": email}, {
+                                      "following": 1})[0]["following"]
+        retArr = []
+        myFollowers = []
+        if username == None:
+            myFollowers = list(aggregationQuery(collection="user", match={"email": email}, lookup={"from": "user", "localField": "following", "foreignField": "_id", "as": "followingData", "pipeline": [
+                               {"$project": {"username": 1, "name": 1, "_id": 1}}]}, projection={"followingData": 1}))[0]["followingData"]
         else:
-            for x in myfollowing:
-                name=findWithProject("user",{"username":x},{"name":1})[0]["name"]
-                temObj={
-                    "username":x,
-                    "name":name,
-                    "isFollowing":True
-                }
-                retArr.append(temObj)
-        return {"following":retArr},200
+            myFollowers = list(aggregationQuery(collection="user", match={"username": username}, lookup={"from": "user", "localField": "following", "foreignField": "_id", "as": "followingData", "pipeline": [
+                               {"$project": {"username": 1, "name": 1, "_id": 1}}]}, projection={"followingData": 1}))[0]["followingData"]
+        for x in myFollowers:
+            temObj = {
+                "name": x["name"],
+                "username": x["username"]
+            }
+            if x["_id"] in myFollowing:
+                temObj["isFollowing"] = True
+            else:
+                temObj["isFollowing"] = False
+            retArr.append(temObj)
+
+        return {"following": retArr}, 200
+
 
 class Tag(Resource):
     @is_registerd
-    def get(email,self,username=None):
-        myTags=findWithProject("user",{"email":email},{"tags":1})[0]["tags"]
-        retArr=[]
-        if(username!=None):
-            tags=findWithProject("user",{"username":username},{"tags":1})[0]["tags"]
+    def get(email, self, username=None):
+        myTags = findWithProject("user", {"email": email}, {
+                                 "tags": 1})[0]["tags"]
+        retArr = []
+        if (username != None):
+            tags = findWithProject("user", {"username": username}, {
+                                   "tags": 1})[0]["tags"]
             for x in tags:
-                temObj={
-                    "tag":x
+                temObj = {
+                    "tag": x
                 }
                 if x in myTags:
-                    temObj["isFollowing"]=True
+                    temObj["isFollowing"] = True
                 else:
-                    temObj["isFollowing"]=False
+                    temObj["isFollowing"] = False
                 retArr.append(temObj)
         else:
             for x in myTags:
-                temObj={
-                    "tag":x,
-                    "isFollowing":True
+                temObj = {
+                    "tag": x,
+                    "isFollowing": True
                 }
                 retArr.append(temObj)
-        return {"tags":retArr},200
+        return {"tags": retArr}, 200
+
 
 class TagFollow(Resource):
     @is_registerd
-    def post(email,self):
-        data=request.json
-        tag=data["tag"]
-        requestUserName=request.headers.get("username");
-        updateOne("user",{"$push":{"tags":tag}},{"username":requestUserName})
-        return {"status":"success"},200
+    def post(email, self):
+        data = request.json
+        tag = data["tag"]
+        updateOne("user", {"$push": {"tags": tag}}, {"email": email})
+        return {"status": "success"}, 200
 
 
 class TagUnFollow(Resource):
     @is_registerd
-    def post(email,self):
-        data=request.json
-        tag=data["tag"]
-        requestUserName=request.headers.get("username");
-        updateOne("user",{"$pullAll":{"tags":[tag]}},{"username":requestUserName})
-        return {"status":"success"},200
+    def post(email, self):
+        data = request.json
+        tag = data["tag"]
+        updateOne("user", {"$pullAll": {"tags": [tag]}}, {"email": email})
+        return {"status": "success"}, 200
