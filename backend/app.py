7e6d8e71163b88b3b874565a7f2eeb203876d521
Jayashakthi Vishnu <vishnu28.js@gmail.com>
2023-02-25 06:03:30 +0530

    integrate notification route

--- backend/app.py
+++ backend/app.py
@@ -5,6 +5,8 @@ from flask_restful import Api
 from routes.Auth import Register
 from routes.Post import Post
 from routes.Post import FollowingVisited
+from routes.Post import Like
+from routes.Post import UnLike
 from routes.Post import Random
 from routes.Post import Visited
 from routes.Post import AllPosts
@@ -17,6 +19,7 @@ from routes.User import Follow
 from routes.User import UnFollow
 from routes.User import TagFollow
 from routes.User import TagUnFollow
+from routes.User import Notification
 from routes.UserName import UserName
 from routes.User import Followers
 from routes.User import Following
@@ -34,8 +37,11 @@ app.secret_key = 'the random string'
 
 api.add_resource(Register,"/register")
 api.add_resource(AllPosts,"/allpost","/allpost/<username>")
+api.add_resource(Like,"/like")
+api.add_resource(UnLike,"/unlike")
 api.add_resource(Visited,"/visited/<postId>")
 api.add_resource(Comments,"/comments/<postId>")
+api.add_resource(Notification,"/notification/<page>")
 api.add_resource(FollowingVisited,"/post/visited/<page>")
 api.add_resource(Random,"/post/random/<page>")
 api.add_resource(Post,"/post")
